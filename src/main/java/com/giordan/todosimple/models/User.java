1cf215610272e8c4562a02fd0c24b4e27d1eceea
Giordan Garcia <giordangarcia12@gmail.com>
2023-09-24 11:35:38 -0300

    Create User model

---
+++ src/main/java/com/giordan/todosimple/models/User.java
@@ -0,0 +1,45 @@
+package com.giordan.todosimple.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.ArrayList;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public interface CreateUser {
+    }
+
+    public interface UpdateUser {
+    }
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotBlank(groups = CreateUser.class)
+    @Size(min = 2, max = 100, groups = CreateUser.class)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    @Size(min = 8, max = 60, groups = {CreateUser.class, UpdateUser.class})
+    private String password;
+
+//    private List<Task> tasks = new ArrayList<Task>();
+
+
+}
