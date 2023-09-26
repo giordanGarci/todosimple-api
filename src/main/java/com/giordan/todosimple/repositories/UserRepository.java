9468de0582ee0a632de0b3d453b76791b2764085
Giordan Garcia <giordangarcia12@gmail.com>
2023-09-26 15:22:43 -0300

    Create User and Task repositories

---
+++ src/main/java/com/giordan/todosimple/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.giordan.todosimple.repositories;
+
+import com.giordan.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
