760f6712867bbdaa995029e7b8f24aba1aaa44b3
Giordan Garcia <giordangarcia12@gmail.com>
2023-10-06 11:34:13 -0300

    Create UserController

--- src/main/java/com/giordan/todosimple/models/User.java
+++ src/main/java/com/giordan/todosimple/models/User.java
@@ -1,5 +1,6 @@
 package com.giordan.todosimple.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
@@ -11,7 +12,6 @@ import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter @Setter
 @EqualsAndHashCode
 @Entity
 @Table(name = User.TABLE_NAME)
@@ -27,21 +27,28 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
+    @Getter @Setter
     private Long id;
 
     @Column(name = "username", length = 100, nullable = false, unique = true)
     @NotBlank(groups = CreateUser.class)
     @Size(min = 2, max = 100, groups = CreateUser.class)
+    @Getter @Setter
     private String username;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
     @NotBlank(groups = {CreateUser.class, UpdateUser.class})
     @Size(min = 8, max = 60, groups = {CreateUser.class, UpdateUser.class})
+    @Getter @Setter
     private String password;
 
     @OneToMany(mappedBy = "user")
     List<Task> tasks = new ArrayList<>();
 
+    @JsonIgnore
+    public List<Task> getTasks() {
+        return tasks;
+    }
 
 }
