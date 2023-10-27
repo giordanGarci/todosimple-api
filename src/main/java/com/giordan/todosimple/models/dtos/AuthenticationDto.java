2f0daa695e9c4bd21f922c6034ad88517733a6f2
Giordan Garcia <giordangarcia12@gmail.com>
2023-10-27 10:18:48 -0300

    Delete useless classes

--- src/main/java/com/giordan/todosimple/models/dtos/AuthenticationDto.java
+++
@@ -1,4 +0,0 @@
-package com.giordan.todosimple.models.dtos;
-
-public record AuthenticationDto(String login, String password) {
-}
--- src/main/java/com/giordan/todosimple/models/dtos/RegisterDto.java
+++
@@ -1,4 +0,0 @@
-package com.giordan.todosimple.models.dtos;
-
-public record RegisterDto(String login, String password, String role) {
-}
